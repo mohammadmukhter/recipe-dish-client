104faa6a6a0db0930fd370a8213ebf1ffb263259
MUKHTER <mohammadmukhter@gmail.com>
2023-05-02 13:09:18 +0600

    banner section finally done

--- src/components/pages/shared/Banner.jsx
+++ src/components/pages/shared/Banner.jsx
@@ -1,29 +1,102 @@
 import React from "react";
-import bannerImage from "../../../assets/banner1.jpg";
+import bannerImage1 from "../../../assets/banner1.jpg";
+import bannerImage2 from "../../../assets/banner2.jpg";
+import bannerImage3 from "../../../assets/banner3.jpg";
 
 const Banner = () => {
   return (
-    <div>
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage: `url(${bannerImage})`,
-        }}
-      >
-        <div className=""></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+    <>
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <div
+            className="hero min-h-screen relative"
+            style={{ backgroundImage: `url(${bannerImage1})` }}
+          >
+            <div className=" bg-opacity-60"></div>
+            <div className="absolute left-[100px] top-50% hero-content text-left text-neutral-content">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">
+                  Spice Up Your Kitchen with Indian Recipes
+                </h1>
+                <p className="mb-5">
+                  Bring the tantalizing tastes of India to your home kitchen.
+                  Discover a treasure trove of easy-to-follow recipes that will
+                  awaken your taste buds with bold spices, fragrant herbs, and
+                  unique combinations of flavors. Experience the magic of Indian
+                  cooking at your fingertips.
+                </p>
+                <button className="btn btn-primary">Explore More</button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <div
+            className="hero min-h-screen relative"
+            style={{ backgroundImage: `url(${bannerImage2})` }}
+          >
+            <div className=" bg-opacity-60"></div>
+            <div className="absolute left-[100px] top-50% hero-content text-left text-neutral-content">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">
+                  Experience the Essence of India
+                </h1>
+                <p className="mb-5">
+                  Immerse yourself in the diverse and colorful world of Indian
+                  cuisine. Uncover the secrets of
+                </p>
+                <button className="btn btn-primary">Explore more</button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <div
+            className="hero min-h-screen relative"
+            style={{ backgroundImage: `url(${bannerImage3})` }}
+          >
+            <div className=" bg-opacity-60"></div>
+            <div className="absolute left-[100px] top-50% hero-content text-left text-dark">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">
+                  Discover the Flavors of India
+                </h1>
+                <p className="mb-5">
+                  Experience the vibrant and diverse cuisine of India with our
+                  collection of authentic recipes. From spicy curries to savory
+                  snacks
+                </p>
+                <button className="btn btn-primary">Explore more</button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/pages/shared/NavBar.jsx
+++ src/components/pages/shared/NavBar.jsx
@@ -6,7 +6,7 @@ import Banner from "./Banner";
 const NavBar = () => {
   return (
     <>
-      <div className="navbar bg-gray-500 text-white px-0">
+      <div className="navbar bg-stone-800 text-white px-0 sticky top-0 z-50">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
